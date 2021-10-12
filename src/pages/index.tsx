fdefd6ae2a829506bbf649c78732235030c9438a
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-12 13:27:10 -0300

    Enhancement index.tsx Structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
-import type { NextPage } from 'next';
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
-import PageTitle from 'ui/components/data-display/pageTitle/PageTitle';
+import type { NextPage } from "next";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/pageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/user-information/UserInformation";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <SafeEnvironment/>
+      <SafeEnvironment />
       <PageTitle
-        title={'Conheça os profissionais'}
-        subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+      <UserInformation
+        name={"Brenda Cordeiro"}
+        picture={"https://github.com/BrendaRodriguees.png"}
+        rating={5}
+        description={"Nova Lima"}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
