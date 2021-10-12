aa48f30a4ee82c40cd49059ac0138729fc226a0f
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-12 13:28:54 -0300

    Enhancement Safe Environment Component Structure

--- src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { SafeEnvironmentContainer } from './SafeEnvironment.style';
-import { Container } from '@mui/material'; // Limit screen size
+import React from "react";
+import { SafeEnvironmentContainer } from "./SafeEnvironment.style";
+import { Container } from "@mui/material"; // Limit screen size
 
 // function that will be the component
 // All component needs to have Upper case to React knows that it's a component
 const SafeEnvironment = () => {
-    return(
-        <SafeEnvironmentContainer>
-            <Container>
-                Safe environment
-            </Container>
-        </SafeEnvironmentContainer>
-    );
+  return (
+    <SafeEnvironmentContainer>
+      <Container>
+        Safe environment <i className={"twf-lock"} />
+      </Container>
+    </SafeEnvironmentContainer>
+  );
 };
 
 export default SafeEnvironment;
