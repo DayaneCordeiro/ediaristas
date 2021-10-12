3ea93dbf637a0c401a40031f991c81f1366e4dd9
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-12 13:28:09 -0300

    Enhancement Page Title Component Structure

--- src/ui/components/data-display/pageTitle/PageTitle.tsx
+++ src/ui/components/data-display/pageTitle/PageTitle.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { PageTitleContainer, PageTitleStyled } from './PageTitle.style';
+import React from "react";
+import { PageTitleContainer, PageTitleStyled } from "./PageTitle.style";
 
 interface PageTitleProps {
-    title: string;
-    subtitle?: string | JSX.Element; // ? <- define the property not required. JSX.Element allows to call HTML elements like parameter
+  title: string;
+  subtitle?: string | JSX.Element; // ? <- define the property not required. JSX.Element allows to call HTML elements like parameter
 }
 
 // React FC Type -> Funtional Component
 const PageTitle: React.FC<PageTitleProps> = (props) => {
-    return (
-        <PageTitleContainer>
-            <PageTitleStyled>
-                {props.title}
-            </PageTitleStyled>
-            {props.subtitle}
-        </PageTitleContainer>)
-    ;
-}
+  return (
+    <PageTitleContainer>
+      <PageTitleStyled>{props.title}</PageTitleStyled>
+      {props.subtitle}
+    </PageTitleContainer>
+  );
+};
 
 export default PageTitle;
