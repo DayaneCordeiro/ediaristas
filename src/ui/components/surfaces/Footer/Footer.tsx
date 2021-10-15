cd62f7cae81de746b0e3a3f58465b29b91e42930
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-15 14:26:23 -0300

    Feature Footer Component

---
+++ src/ui/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Footer.style";
+import { Typography, Box } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle>Quem somos</FooterTitle>
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
+            adipisci vel dolore natus odit laboriosam eius eaque possimus maxime
+            rem temporibus quae quibusdam beatae perferendis harum est, impedit
+            voluptatum quis.
+          </Typography>
+        </Box>
+        <div>
+          <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"nooperner noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"App Store"} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"nooperner noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"Google Play"} />
+              </a>
+            </li>
+          </AppList>
+        </div>
+      </FooterContainer>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
