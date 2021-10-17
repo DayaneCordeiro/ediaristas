a372c1a1cdb07549505273276476eeae5bf23c91
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-17 20:33:47 -0300

    Feature TextField and TextFieldMask Components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/pageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/user-information/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,14 @@ const Home: NextPage = () => {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
+
+      <TextFieldMask
+        mask={"99.999-999"}
+        label="Digite seu CEP"
+        fullWidth
+        variant={"outlined"}
+      />
+
       <UserInformation
         name={"Brenda Cordeiro"}
         picture={"https://github.com/BrendaRodriguees.png"}
